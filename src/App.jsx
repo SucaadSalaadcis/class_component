e16ed3f5d7e95f90b21f3aed5a0de4b0d2af7050
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2024-04-02 02:46:55 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>Old version of using react js class component</div>
+    )
+  }
+}
+
+export default App
